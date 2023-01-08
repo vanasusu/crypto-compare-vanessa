8272e54e8b6e39dbfe9a06ddf8bda0ffe9c3dc30
Vanessa <vanasusu@gmail.com>
2023-01-09 01:09:36 +0530

    Commented out code for better readability

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ let cryptoData = []
 let barChartRef = null
 let lineChartRef = null
 let pieChartRef = null
+//generating chart using ChartJS
 function generateChart(canvasId, chartType, labels, data) {
 	const ctx = document.getElementById(canvasId);
 	let datasetObj = {
@@ -12,11 +13,11 @@ function generateChart(canvasId, chartType, labels, data) {
 		borderColor: '#4fa060',
 		backgroundColor: '#4fa060',
 	}
-	if(chartType === "pie"){
+	if(chartType === "pie"){ //for pie chart remove custom borderColor and backgroundColor and use default
 		delete datasetObj.borderColor
 		delete datasetObj.backgroundColor
 	}
-	const _chart = new Chart(ctx, {
+	const _chart = new Chart(ctx, { //initiating chart js instance
 		type: chartType,
 		data: {
 			labels,
@@ -32,6 +33,7 @@ function generateChart(canvasId, chartType, labels, data) {
 			}
 		}
 	});
+	//saving chart instance globally for updating chart data later on
 	if(chartType === "bar"){
 		barChartRef = _chart
 	}else if(chartType === "line"){
@@ -48,11 +50,11 @@ function setTableData(rows) { //function to feed table rows with data
 			$('<td>').text(item.name),
 			$('<td class="price">').text(`$${item.quote?.USD?.price?.toFixed(2)}`),
 			$('<td class="volume">').text(`${item.quote?.USD?.volume_24h?.toFixed(2)}`),
-			$(`<td class="change_percentage ${change > 0 ? "text-success" : "text-danger"}">`).text(`${change?.toFixed(2)}%`), //change color based on percentage change
+			$(`<td class="change_percentage ${change > 0 ? "text-success" : change < 0 ? "text-danger" : "text-muted"}">`).text(`${change?.toFixed(2)}%`), //change color based on percentage change
 			$('<td class="ticker d-none">').text(item.symbol),
 		);
 		$tr.wrap('<p>').html();
-		$tr.appendTo('#crypTable')
+		$tr.appendTo('#crypTable');
 	});
 }
 async function fetchCryptoList() {
@@ -60,7 +62,7 @@ async function fetchCryptoList() {
 		//show loading
 		$(".table-loader").removeClass("d-none")
 		//fetch crypto prices using axios from coinapi REST API
-		//https://pro-api.coinmarketcap.com
+		//https://sandbox-api.coinmarketcap.com
 		const resp = await axios.get("https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest", {
 			headers: {
 				"X-CMC_PRO_API_KEY": "f5c41f87-276c-4f7f-8beb-8fa0e59949de"
@@ -72,7 +74,7 @@ async function fetchCryptoList() {
 			//append rows in the table
 			setTableData(cryptoData)
 			$('#crypTable').prepend($('<tr><td colspan="4" class="text-center table-loader"><div class="spinner-border text-light" role="status"></div></td></tr>'));
-			
+
 			//hide loading after loading of data
 			$(".table-loader").addClass("d-none")
 			//load tradingview chart with the first ticker
@@ -93,17 +95,18 @@ async function fetchCryptoList() {
 			);
 			const labels = cryptoData.map(d => d.name)
 			const data = cryptoData.map(d => d.quote?.USD?.price)
+			//generating bar chart
 			generateChart("bar_chart", "bar", labels, data)
+			//generating line chart
 			generateChart("line_chart", "line", labels, data)
+			//generating pie chart
 			generateChart("pie_chart", "pie", labels.slice(0, 20), data.slice(0, 20))
 		}
 	} catch (err) {
 
 	}
 }
-$(document).ready(function () {
-	fetchCryptoList()
-});
+//table row click event listner
 $('#crypTable').on('click', 'tbody tr', function (event) {
 	var ticker = $(event.target).closest('tr').find(".ticker").html();
 	//reload tradingview instance with selected ticker
@@ -122,8 +125,10 @@ $('#crypTable').on('click', 'tbody tr', function (event) {
 			"container_id": "tradingview_main"
 		}
 	);
+	//highlighting selected row
 	$(this).addClass('highlight').siblings().removeClass('highlight');
 });
+//keyup event listner for search functionality
 $("#search_text").keyup(function (event) {
 	let text = $(this).val();
 	let tempData = []
@@ -135,6 +140,7 @@ $("#search_text").keyup(function (event) {
 	}
 	const labels = tempData.map(d => d.name)
 	const data = tempData.map(d => d.quote?.USD?.price)
+	//update table data
 	setTableData(tempData)
 	//update bar chart
 	barChartRef.data.labels = labels;
@@ -148,4 +154,8 @@ $("#search_text").keyup(function (event) {
 	pieChartRef.data.labels = labels;
 	pieChartRef.data.datasets[0].data = data;
 	pieChartRef.update();
+});
+//load crypto data on page load
+$(document).ready(function () {
+	fetchCryptoList()
 });
